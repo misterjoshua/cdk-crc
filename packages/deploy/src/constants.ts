2ea4fdbd08d59339da548bd82e635e797ee179ae
Josh Kellendonk <joshkellendonk@gmail.com>
2021-08-26 14:49:15 -0600

    add pipelines

--- packages/deploy/src/constants.ts
+++ packages/deploy/src/constants.ts
@@ -1,3 +1,8 @@
 import * as path from 'path';
 
 export const PACKAGES_BASE = path.join(__dirname, '..', '..');
+
+// Change this to whatever.
+export const PIPELINE_CONNECTION_ID = '42ae9e77-996d-4371-a5ab-22a03487e51d';
+export const PIPELINE_REPO = 'misterjoshua/cdk-crc';
+export const PIPELINE_REPO_BRANCH = 'main';
--- packages/deploy/src/pipeline.ts
+++ packages/deploy/src/pipeline.ts
@@ -5,22 +5,35 @@ import {
   ShellStep,
 } from '@aws-cdk/pipelines';
 import { CdkCrcStage } from './cdk-crc-stage';
+import {
+  PIPELINE_CONNECTION_ID,
+  PIPELINE_REPO,
+  PIPELINE_REPO_BRANCH,
+} from './constants';
 
 export class Pipeline extends cdk.Stack {
   constructor(app: cdk.Construct, id: string, props: cdk.StackProps = {}) {
     super(app, id, props);
 
+    const connectionArn = `arn:aws:codestar-connections:${this.region}:${this.account}:connection/${PIPELINE_CONNECTION_ID}`;
+
     const pipeline = new CodePipeline(this, 'Pipeline', {
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('misterjoshua/cdk-crc', 'main'),
+        input: CodePipelineSource.connection(
+          PIPELINE_REPO,
+          PIPELINE_REPO_BRANCH,
+          {
+            connectionArn,
+          },
+        ),
         commands: ['npm i -g yarn', 'yarn', 'yarn run build'],
         primaryOutputDirectory: 'packages/deploy/cdk.out',
       }),
 
       // Allow the pipeline to self-update from git.
-      selfMutation: true,
+      selfMutation: false,
 
-      // Docker is used throughout synths.
+      // Docker is used throughout.
       dockerEnabledForSynth: true,
       dockerEnabledForSelfMutation: true,
     });
