ff24ec8024dba5da3709e866342951039ccd68fc
Josh Kellendonk <joshkellendonk@gmail.com>
2021-08-26 19:52:12 -0600

    add a hit counter to the site

---
+++ packages/frontend/src/components/hit-counter.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+function useHitCounter() {
+  const [hitCounter, setHitCounter] = useState<number | undefined>();
+
+  useEffect(() => {
+    async function func() {
+      const host = /localhost/.test(window.location.host)
+        ? 'https://d3868n22s0idtt.cloudfront.net/api/hits'
+        : '/api/hits';
+
+      const res = await fetch(host);
+      const resJson = await res.json();
+      const hitCount = resJson.hitCount;
+      setHitCounter(hitCount);
+    }
+
+    func();
+  }, [setHitCounter]);
+
+  return hitCounter;
+}
+
+export const HitCounter: React.FC = () => {
+  const hits = useHitCounter();
+
+  if (hits !== undefined) {
+    return <>This site has {hits} hits</>;
+  } else {
+    return <></>;
+  }
+};
--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { HitCounter } from '../components/hit-counter';
+
 export default function Home() {
-  return <p>Hello</p>;
+  return (
+    <>
+      <h1>Cloud Resume Challenge</h1>
+      <p>Welcome to my Cloud Resume Challenge, prepared in CDK.</p>
+      <HitCounter />
+    </>
+  );
 }
