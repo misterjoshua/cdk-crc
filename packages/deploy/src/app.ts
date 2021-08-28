bc5f694e080c3edcc7e852d5eecfc116b45571ae
Josh Kellendonk <joshkellendonk@gmail.com>
2021-08-28 13:39:41 -0600

    clean up the app

--- packages/deploy/src/app.ts
+++ packages/deploy/src/app.ts
@@ -2,31 +2,27 @@
 import * as cdk from '@aws-cdk/core';
 import 'source-map-support/register';
 import { CdkCrcStage } from './cdk-crc-stage';
-import {
-  DOMAIN_CERT_PARAM,
-  DOMAIN_NAME,
-  DOMAIN_ZONE_ID_PARAM,
-} from './constants';
+import * as constants from './constants';
 import { Pipeline } from './pipeline';
 
 const app = new cdk.App();
 
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+// Development environment.
 new CdkCrcStage(app, 'CdkCrc-Dev', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+  env,
   domainConfig: {
-    certificateParameter: DOMAIN_CERT_PARAM,
-    domainNames: [`cdk-crc-dev.${DOMAIN_NAME}`],
-    hostedZoneIdParameter: DOMAIN_ZONE_ID_PARAM,
+    certificateParameter: constants.DOMAIN_CERT_PARAM,
+    domainNames: [`cdk-crc-dev.${constants.DOMAIN_NAME}`],
+    hostedZoneIdParameter: constants.DOMAIN_ZONE_ID_PARAM,
   },
 });
 
-// Continuous Delivery pipeline
+// Continuous Delivery pipeline (uses CdkCrcStage)
 new Pipeline(app, 'CdkCrc-Pipeline', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+  env,
 });
